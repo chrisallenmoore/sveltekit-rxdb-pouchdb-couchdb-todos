127de1f84a272e88a7b01b179d3d0fcb4dabb3d1
Chris Allen Moore <chris@chrisallenmoore.com>
2022-02-04 15:04:24 +0700

    added rxDB colors to Tailwind CSS config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'rxdb-midnight-blue': '#1A202C',
+        'rxdb-midnight-dark': '#171923',
+        'rxdb-pink': '#E6008D',
+        'rxdb-fuchsia': '#8D2089',
+        'rxdb-purple': '#5F2688',
+
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
